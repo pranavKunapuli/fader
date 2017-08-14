81573f63870aef8791eb875ac14ee74eeb9359fa
Pranav Kunapuli <pranavtkunapuli@gmail.com>
2017-08-14 19:58:40 -0400

    Main server file

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var port = 8000;
+
+var app = express();
+
+
+app.listen(port, function() {
+	console.log("Started server listening on port " + port);
+});
