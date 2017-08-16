c73811e1349725ae509705778dd93d2c1f561202
Pranav Kunapuli <pranavtkunapuli@gmail.com>
2017-08-15 21:58:30 -0400

    Routing

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 
-var port = 8000;
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser'); // Request body parser
+var session = require('express-session');
+
+var routes = require('./app/routes.js');
+
+var port = 8000; // TCP port for incoming connections
 
 var app = express();
 
-app.use(bodyParser());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
+app.use("/", routes);
+
+// Listening on specified port
 app.listen(port, function() {
 	console.log("Started server listening on port " + port);
 });
